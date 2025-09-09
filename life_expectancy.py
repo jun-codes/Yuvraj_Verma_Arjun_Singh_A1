6797fb4498dfe4a29592e30a2e8e399d4a5f6c5b
Arjun <arjundoescardistry@gmail.com>
2025-09-09 15:51:30 +0530

    india plot

--- life_expectancy.py
+++ life_expectancy.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 data = pd.read_csv('life expectancy.csv')
-plt.scatter(data['Year'], data['Life expectancy '])
+india = data[data['Country'] == 'India']
+plt.scatter(india['Year'], india['Life expectancy '], marker='o')
+plt.xlabel("Year")
+plt.ylabel("Life Expectancy")
+plt.title("Life Expectancy of India over Years")
 plt.show()
