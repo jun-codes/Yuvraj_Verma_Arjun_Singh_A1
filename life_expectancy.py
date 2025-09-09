5e398c402e141eb28ebdf19a280bc18c95a6a1cf
Arjun <arjundoescardistry@gmail.com>
2025-09-09 18:50:00 +0530

    linear only using year

--- life_expectancy.py
+++ life_expectancy.py
@@ -16,12 +16,16 @@ def loss_function(m, b, points):
         total_error += (y - (m * x + b))** 2
     return total_error / float(len(points))
 
+india = data[data['Country'] == 'India'].copy()
+
+india['Year_scaled'] = (india['Year'] - 2000) /10
+
 def gradient_descent(m_now, b_now, points, learning_rate):
     m_gradient = 0
     b_gradient = 0
     N = float(len(points))
     for i in range(len(points)):
-        x = points.iloc[i].Year
+        x = points.iloc[i].Year_scaled
         y = points.iloc[i]['Life expectancy ']
         m_gradient += -(2/N) * x * (y - (m_now * x + b_now))
         b_gradient += -(2/N) * (y - (m_now * x + b_now))
@@ -31,16 +35,24 @@ def gradient_descent(m_now, b_now, points, learning_rate):
 
 m = 0
 b = 0
-learning_rate = 0.0000001
-num_iterations = 1000
+learning_rate = 0.01  
+num_iterations = 5000
+
 for i in range(num_iterations):
     m, b = gradient_descent(m, b, india, learning_rate)
+    if i % 500 == 0:
+        print(f"Iteration {i}: slope m={m:.4f}, intercept b={b:.4f}")
 
-plt.scatter(india['Year'], india['Life expectancy '], marker='o')
-plt.plot(india['Year'], [m*x + b for x in india['Year']], color='red')
+
+plt.scatter(india['Year'], india['Life expectancy '], marker='o', label='Actual Data')
+
+predicted_y = [m * ((x - 2000) / 10) + b for x in india['Year']]
+plt.plot(india['Year'], predicted_y, color='red', label='Linear Fit')
+
 plt.xlabel("Year")
 plt.ylabel("Life Expectancy")
 plt.title("Linear Fit for India Life Expectancy")
+plt.legend()
 plt.show()
 
 print("done")
