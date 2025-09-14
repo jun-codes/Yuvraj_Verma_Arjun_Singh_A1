d366974ef4118fcaaddc1fb4fcc68af63272fda2
Arjun <arjundoescardistry@gmail.com>
2025-09-15 00:18:26 +0530

    final reports

--- life_expectancy_task/src/train_model.py
+++ life_expectancy_task/src/train_model.py
@@ -7,7 +7,7 @@ from data_preprocessing import preprocess_data
 
 raw_data = pd.read_csv('data/train_data.csv')
 raw_data.columns = raw_data.columns.str.strip()
-data = preprocess_data(raw_data)
+data = preprocess_data(raw_data) #don't commit data   
 
 def loss_function(weights, bias, features, target):
     y_predicted = predict(features, weights, bias)
@@ -17,7 +17,7 @@ def gradient_descent(weights_now, bias_now, features, target, learning_rate):
     N = features.shape[0]
     y_predicted = predict(features, weights_now, bias_now)
     errors = y_predicted - target
-    weight_gradients = (2/N) * np.dot(features.T, errors)
+    weight_gradients = (2/N) * np.dot(features.T, errors) #vectorized using numpy = quicker
     bias_gradient = (2/N) * np.sum(errors)
     new_weights = weights_now - learning_rate * weight_gradients
     new_bias = bias_now - learning_rate * bias_gradient
@@ -32,7 +32,7 @@ country_features = list(data.filter(like="Country_").columns)
 selected_features_list = [
     'Income composition of resources', 'Schooling', 'HIV/AIDS', 'Adult Mortality',
     'BMI', 'Diphtheria', 'Polio', 'thinness  1-19 years', 'GDP', 'Alcohol'
-] + country_features + status_features
+] + country_features + status_features #satus/country 1 hot encoded
 
 features = data[selected_features_list]
 target = data['Life expectancy']
@@ -45,7 +45,7 @@ num_features = X_train.shape[1]
 weights = np.zeros(num_features)
 bias = 0
 learning_rate = 0.15
-num_iterations = 50000
+num_iterations = 50000 # didn't require this many plateus around 30k
 
 for i in range(num_iterations):
     weights, bias = gradient_descent(weights, bias, X_train, y_train, learning_rate)
@@ -65,7 +65,7 @@ plt.xlabel("Actual life Expectancy")
 plt.ylabel("Predicted life Expectancy")
 plt.title("Actual vs Predicted life Expectancy")
 plt.legend()
-plt.grid(True)
+plt.grid(True) #graph with true fit line
 plt.show()
 print("done")
 
@@ -74,4 +74,4 @@ model3 = {"weights": weights, "bias": bias, "features": selected_features_list}
 os.makedirs("life_expectancy_task/models", exist_ok=True)
 
 with open("life_expectancy_task/models/regression_model3.pkl", "wb") as f:
-    pickle.dump(model3, f)
+    pickle.dump(model3, f) #byte steam pickle file
